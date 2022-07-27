479e2dc7f84d794c4d1520d8622ceb5893520eb9
bertolucci-alan <bertoluccialan23@gmail.com>
2022-07-26 22:29:38 -0300

    feat: create update movie method

--- src/modules/movies/controllers/MovieController.ts
+++ src/modules/movies/controllers/MovieController.ts
@@ -1,11 +1,13 @@
 import { Movies } from "@prisma/client";
-import { Authorized, Body, CurrentUser, Get, JsonController, Post, QueryParam, QueryParams } from "routing-controllers";
+import { Authorized, Body, CurrentUser, Get, JsonController, Param, Post, Put, QueryParam, QueryParams } from "routing-controllers";
 import { container } from "tsyringe";
 import { Session } from "../../../shared/types/Session";
 import { CreateMovieDTO } from "../dtos/CreateMovieDTO";
+import { UpdateMovieDTO } from "../dtos/UpdateMovieDTO";
 import { IRequestFindAllMovie } from "../repositories/interfaces/IMovieRepository";
 import { CreateMovieUseCase } from "../useCases/createMovie/CreateMovieUseCase";
 import { ListMoviesUseCase } from "../useCases/listMovies/ListMoviesUseCase";
+import { UpdateMovieUseCase } from "../useCases/updateMovie/UpdateMovieUseCase";
 
 @JsonController("/movies")
 export class MovieController {
@@ -17,6 +19,17 @@ export class MovieController {
     ): Promise<Movies> {
         const createMovieUseCase = container.resolve(CreateMovieUseCase);
         return await createMovieUseCase.execute(body, authUser);
+    }
+
+    @Authorized()
+    @Put("/:movie_id")
+    async update(
+        @Body() body: UpdateMovieDTO,
+        @Param('movie_id') movie_id: string,
+        @CurrentUser() authUser: Session
+    ): Promise<Movies> {
+        const updateMovieUseCase = container.resolve(UpdateMovieUseCase);
+        return await updateMovieUseCase.execute(movie_id, body, authUser.id);
     }
 
     @Get("/")
